924fb8d9dde8ac73749d9cc8edf091f251407208
lucyhecht1 <lucyhecht2@gmail.com>
2025-03-23 10:52:33 -0400

    Logo visibility

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ export default function Header() {
     <header className="garage-header">
       <div className="garage-header-content">
         <a href="https://www.withgarage.com/" target="_blank" rel="noopener noreferrer">
-          <img src="logo-2.jpeg" alt="Garage Logo" className="garage-logo" />
+          <img src={`${process.env.PUBLIC_URL}/logo-2.jpeg`} alt="Garage Logo" className="garage-logo" />
         </a>
         <h1 className="garage-title">Garage Invoice Generator</h1>
       </div>
